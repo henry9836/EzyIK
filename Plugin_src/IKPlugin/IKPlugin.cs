d35a92f7737ce2393f7bbbcb531e740dceac6b49
Henry Oliver <henry983615@gmail.com>
2020-04-30 15:52:31 +1200

    Inital Push

---
+++ Plugin_src/IKPlugin/IKPlugin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace HenryIK
+{
+    public class IKPlugin
+    {
+        public static void Test()
+        {
+            UnityEngine.Debug.Log("Hello Debug!");
+        }
+    }
+}
---
+++ Project/IKPlugin/Assets/Scripts/Invoker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HenryIK;
+
+public class Invoker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        IKPlugin.Test();
+    }
+
+}
