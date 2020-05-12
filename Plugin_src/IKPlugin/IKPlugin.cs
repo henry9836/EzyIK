8ffd2df54907e4f561f0efd9da2523b7dd14e10e
Henry Oliver <henry983615@gmail.com>
2020-05-12 21:29:34 +1200

    basic struct

--- Plugin_src/IKPlugin/IKPlugin.cs
+++ Plugin_src/IKPlugin/IKPlugin.cs
@@ -5,6 +5,36 @@ namespace HenryIK
 {
     public class IKPlugin
     {
+
+        struct BoneNode
+        {
+
+            public GameObject node;
+            public Transform nodeTransform;
+            public Transform parentTransform;
+            public float initalDistanceFromParent;
+            public bool isRootNode;
+
+            public BoneNode(GameObject _node)
+            {
+                node = _node;
+                nodeTransform = node.transform;
+                isRootNode = false;
+                //If we have a parent
+                if (node.transform.parent)
+                {
+                    parentTransform = node.transform.parent.transform;
+                    initalDistanceFromParent = Vector3.Distance(nodeTransform.position, parentTransform.position);
+                }
+                else
+                {
+                    parentTransform = null;
+                    initalDistanceFromParent = 0.0f;
+                    isRootNode = true;
+                }
+            }
+        }
+
         public static void Test()
         {
             UnityEngine.Debug.Log("Hello Debug!");
