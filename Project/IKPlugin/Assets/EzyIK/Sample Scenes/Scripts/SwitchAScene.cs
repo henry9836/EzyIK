ee1dae4fc5591ad8c5e28836326718841e10a85d
Henry Oliver <henry983615@gmail.com>
2020-06-04 23:09:37 +1200

    updated docs

---
+++ Project/IKPlugin/Assets/EzyIK/Sample Scenes/Scripts/SwitchAScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchAScene : MonoBehaviour
+{
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+
+}
